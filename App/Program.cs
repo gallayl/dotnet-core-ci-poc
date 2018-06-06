b29666b7a7981a4f5d55952c7d36bd448fb3e87f
Lajos <gallayl@users.noreply.github.com>
2018-06-06 13:42:29 +0200

    Update Program.cs

--- App/Program.cs
+++ App/Program.cs
@@ -6,6 +6,8 @@ namespace App
     {
 
         public static int Add(int a, int b) => a+b;
+        
+        public static int NotTestedMethodThatShouldDecreaseCoverage(int a, int b) => a-b;
 
         public static void Main()
         {
